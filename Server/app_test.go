5cc1408ed4c5715da2c2fe772cb30e61cbee87f2
paujim <paujim@hotmail.com>
2020-09-15 08:42:20 +1000

    Update app_test.go

--- Server/app_test.go
+++ Server/app_test.go
@@ -59,7 +59,7 @@ func (m *mockedCognitoClient) VerifySoftwareToken(input *cognitoidentityprovider
 	return args.Get(0).(*cognitoidentityprovider.VerifySoftwareTokenOutput), args.Error(1)
 }
 
-func TestServer(t *testing.T) {
+func TestPingURL(t *testing.T) {
 	t.Run("Successful ping", func(t *testing.T) {
 		assert := assert.New(t)
 		mockClient := &mockedCognitoClient{}
@@ -69,6 +69,9 @@ func TestServer(t *testing.T) {
 		assert.NoError(err)
 		assert.Equal(resp.StatusCode, 200)
 	})
+}
+
+func TestTokenURL(t *testing.T) {
 
 	t.Run("Successful token", func(t *testing.T) {
 		assert := assert.New(t)
@@ -84,10 +87,11 @@ func TestServer(t *testing.T) {
 
 		resp, err := http.Post(fmt.Sprintf("%s/v1/token/", ts.URL), "application/json", strings.NewReader(`{"username": "hugo","password": "hugo@password"}`))
 		assert.NoError(err)
+
 		assert.Equal(resp.StatusCode, 200)
 		mockClient.AssertExpectations(t)
 	})
-	t.Run("Validating username and password", func(t *testing.T) {
+	t.Run("Validating token request", func(t *testing.T) {
 		assert := assert.New(t)
 		mockClient := &mockedCognitoClient{}
 		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
@@ -95,10 +99,16 @@ func TestServer(t *testing.T) {
 
 		resp, err := http.Post(fmt.Sprintf("%s/v1/token/", ts.URL), "application/json", strings.NewReader(`{"value1": "hugo","value2": "hugo@password"}`))
 		assert.NoError(err)
-		assert.Equal(resp.StatusCode, 400)
+		defer resp.Body.Close()
+		target := &TokenResponse{}
+		json.NewDecoder(resp.Body).Decode(target)
+
+		assert.Equal(http.StatusBadRequest, resp.StatusCode)
+		assert.Equal("Missing required parameter", *target.Message)
+
 		mockClient.AssertExpectations(t)
 	})
-	t.Run("Error when authenticating", func(t *testing.T) {
+	t.Run("Error when authenticating token request", func(t *testing.T) {
 		assert := assert.New(t)
 		mockClient := &mockedCognitoClient{}
 		mockClient.On("InitiateAuth", mock.Anything).Return(nil, errors.New("ERROR with the password"))
@@ -111,6 +121,76 @@ func TestServer(t *testing.T) {
 		assert.Equal(resp.StatusCode, 401)
 		mockClient.AssertExpectations(t)
 	})
+	t.Run("Successful update", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		mockClient.On("RespondToAuthChallenge", mock.Anything).Return(&cognitoidentityprovider.RespondToAuthChallengeOutput{
+			AuthenticationResult: &cognitoidentityprovider.AuthenticationResultType{
+				AccessToken: aws.String("ACCESS_TOKEN"),
+			},
+		}, nil)
+
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/token/update", ts.URL), "application/json", strings.NewReader(`{"username": "hugo","password": "hugo@password", "session": "SESSION"}`))
+		assert.NoError(err)
+		assert.Equal(resp.StatusCode, 200)
+		mockClient.AssertExpectations(t)
+	})
+	t.Run("Validating update request", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/token/update", ts.URL), "application/json", strings.NewReader(`{"value1": "hugo"}`))
+		assert.NoError(err)
+		defer resp.Body.Close()
+		target := &TokenResponse{}
+		json.NewDecoder(resp.Body).Decode(target)
+
+		assert.Equal(http.StatusBadRequest, resp.StatusCode)
+		assert.Equal("Missing required parameter", *target.Message)
+		mockClient.AssertExpectations(t)
+	})
+	t.Run("Successful code", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		mockClient.On("RespondToAuthChallenge", mock.Anything).Return(&cognitoidentityprovider.RespondToAuthChallengeOutput{
+			AuthenticationResult: &cognitoidentityprovider.AuthenticationResultType{
+				AccessToken: aws.String("ACCESS_TOKEN"),
+			},
+		}, nil)
+
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/token/code", ts.URL), "application/json", strings.NewReader(`{"username": "hugo","code": "123456", "session": "SESSION"}`))
+		assert.NoError(err)
+		assert.Equal(resp.StatusCode, 200)
+		mockClient.AssertExpectations(t)
+	})
+	t.Run("Validating code request", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/token/code", ts.URL), "application/json", strings.NewReader(`{"value1": "hugo"}`))
+		assert.NoError(err)
+
+		defer resp.Body.Close()
+		target := &TokenResponse{}
+		json.NewDecoder(resp.Body).Decode(target)
+
+		assert.Equal(http.StatusBadRequest, resp.StatusCode)
+		assert.Equal("Missing required parameter", *target.Message)
+		mockClient.AssertExpectations(t)
+	})
+}
+
+func TestMFAURL(t *testing.T) {
 
 	t.Run("Success registering MFA", func(t *testing.T) {
 		assert := assert.New(t)
@@ -133,6 +213,85 @@ func TestServer(t *testing.T) {
 		defer ts.Close()
 
 		resp, err := http.Post(fmt.Sprintf("%s/v1/mfa/register", ts.URL), "application/json", strings.NewReader(`{"value": "misssing"}`))
+		assert.NoError(err)
+
+		defer resp.Body.Close()
+		target := &MFAResponse{}
+		json.NewDecoder(resp.Body).Decode(target)
+
+		assert.Equal(http.StatusBadRequest, resp.StatusCode)
+		assert.Equal("Missing required parameter", *target.Message)
+		mockClient.AssertExpectations(t)
+	})
+
+	t.Run("Success enabling MFA", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		mockClient.On("SetUserMFAPreference", mock.Anything).Return(&cognitoidentityprovider.SetUserMFAPreferenceOutput{}, nil)
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/mfa/enable", ts.URL), "application/json", strings.NewReader(`{"accessToken": "ACCESS_TOKEN"}`))
+		assert.NoError(err)
+		assert.Equal(http.StatusOK, resp.StatusCode)
+		mockClient.AssertExpectations(t)
+	})
+	t.Run("Validating enabling MFA", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/mfa/enable", ts.URL), "application/json", strings.NewReader(`{"value": "misssing"}`))
+		assert.NoError(err)
+
+		defer resp.Body.Close()
+		target := &MFAResponse{}
+		json.NewDecoder(resp.Body).Decode(target)
+
+		assert.Equal(http.StatusBadRequest, resp.StatusCode)
+		assert.Equal("Missing required parameter", *target.Message)
+		mockClient.AssertExpectations(t)
+	})
+
+	t.Run("Verifing MFA successfully", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		mockClient.On("VerifySoftwareToken", mock.Anything).Return(&cognitoidentityprovider.VerifySoftwareTokenOutput{Status: aws.String("SUCCESS")}, nil)
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/mfa/verify", ts.URL), "application/json", strings.NewReader(`{"accessToken": "ACCESS_TOKEN", "code": "123456"}`))
+		assert.NoError(err)
+		assert.Equal(http.StatusOK, resp.StatusCode)
+		mockClient.AssertExpectations(t)
+	})
+	t.Run("Failing to verify MFA", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		mockClient.On("VerifySoftwareToken", mock.Anything).Return(nil, errors.New("WRONG CODE"))
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/mfa/verify", ts.URL), "application/json", strings.NewReader(`{"accessToken": "ACCESS_TOKEN", "code": "123456"}`))
+		assert.NoError(err)
+
+		defer resp.Body.Close()
+		target := &MFAResponse{}
+		json.NewDecoder(resp.Body).Decode(target)
+
+		assert.Equal(http.StatusBadRequest, resp.StatusCode)
+		assert.Equal("Unable to verify code", *target.Message)
+
+		mockClient.AssertExpectations(t)
+	})
+	t.Run("Validating verifing MFA", func(t *testing.T) {
+		assert := assert.New(t)
+		mockClient := &mockedCognitoClient{}
+		ts := httptest.NewServer(createApp("userPool", "client", mockClient).Router)
+		defer ts.Close()
+
+		resp, err := http.Post(fmt.Sprintf("%s/v1/mfa/verify", ts.URL), "application/json", strings.NewReader(`{"value": "misssing"}`))
 		assert.NoError(err)
 
 		defer resp.Body.Close()
